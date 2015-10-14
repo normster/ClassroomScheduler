6a6803fe2605b4def21b64f86d2b7b01e737ea39
Norman <thenormanmu@gmail.com>
2015-10-13 18:48:28 -0700

    ready for db entry

--- ScheduleParser.py
+++ ScheduleParser.py
@@ -1,4 +1,5 @@
 import requests
+import sqlite3
 from bs4 import BeautifulSoup
 
 def is_class_info(tag):
@@ -7,12 +8,26 @@ def is_class_info(tag):
     else:
         return True
 
+def main():
+    conn = sqlite3.connect('schedule.db')
+    c = conn.cursor()
+    c.execute("""CREATE TABLE schedule
+            (dept text, num text, name text)""")
 
-payload = {"p_term": "FL", "p_list_all": "Y"}
-r = requests.get('http://osoc.berkeley.edu/OSOC/osoc', params=payload)
+    payload = {"p_term": "FL", "p_list_all": "Y"}
+    r = requests.get('http://osoc.berkeley.edu/OSOC/osoc', params=payload)
 
-soup = BeautifulSoup(r.text, "html.parser")
-tbody = soup.find_all(cellspacing=0)[0] #finds the enlosing tbody tag
-classes = tbody.find_all(is_class_info)
-del classes[0] #remove header row
-print(classes[0].string)
+    soup = BeautifulSoup(r.text, "html.parser")
+    tbody = soup.find_all(cellspacing=0)[0] #finds the enlosing tbody tag
+    classes = tbody.find_all(is_class_info)
+    del classes[0] #remove header row
+
+    for tag in classes:
+        if tag != '\n':
+            print(tag.contents[1].string.strip())
+            print(tag.contents[3].string.strip())
+            print(tag.contents[5].string.strip())
+            print('\n')
+
+if __name__ == "__main__":
+    main()
